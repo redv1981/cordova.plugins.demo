9206b4c542eba4dacf04349ecc89ad3dd8ed8beb
redv1981 <redv1981@gmail.com>
2013-12-17 17:45:23 +0800

    7

--- src/android/DemoActivity.java
+++ src/android/DemoActivity.java
@@ -3,6 +3,7 @@ package cordova.plugins.demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 
 public class DemoActivity extends Activity {
 
@@ -10,7 +11,8 @@ public class DemoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Intent intent = new Intent().putExtra("result", "Heiheihei!");
+		File file = Environment.getDataDirectory();
+		Intent intent = new Intent().putExtra("result", file.toString());
 		setResult(Activity.RESULT_OK, intent);
 		finish();
 	}
