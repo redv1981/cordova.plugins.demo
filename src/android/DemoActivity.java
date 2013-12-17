34977f67d91a10eb49cba9d7c672ef39901e97a3
redv1981 <redv1981@gmail.com>
2013-12-17 17:50:18 +0800

    9

--- src/android/DemoActivity.java
+++ src/android/DemoActivity.java
@@ -13,8 +13,8 @@ public class DemoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		File file = Environment.getDataDirectory();
-		Intent intent = new Intent().putExtra("result", file.toString());
+		File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+		Intent intent = new Intent().putExtra("result", path.toString());
 		setResult(Activity.RESULT_OK, intent);
 		finish();
 	}
