859f2c609683084d9127782a62cb32dd3fa871b6
redv1981 <redv1981@gmail.com>
2013-12-17 17:37:39 +0800

    6

--- src/android/DemoActivity.java
+++ src/android/DemoActivity.java
@@ -2,16 +2,17 @@ package cordova.plugins.demo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 
 public class DemoActivity extends Activity {
 
 	@Override
-	protected void onStop() {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
 		Intent intent = new Intent().putExtra("result", "Heiheihei!");
 		setResult(Activity.RESULT_OK, intent);
 		finish();
-		
-		super.onStop();
 	}
 
 }
