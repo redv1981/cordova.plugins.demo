e174dcfd71a727529bbfd0aed5d01a7cc936bf6b
redv1981 <redv1981@gmail.com>
2013-12-17 17:25:26 +0800

    3

--- src/android/Demo.java
+++ src/android/Demo.java
@@ -5,11 +5,15 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Bundle;
 
 public class Demo extends CordovaPlugin {
+
+	private CallbackContext m_callbackContext;
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -23,9 +27,10 @@ public class Demo extends CordovaPlugin {
 	}
 
 	protected boolean callActivity(String className, CallbackContext callbackContext) {
+		m_callbackContext = callbackContext;
 		Intent intent = new Intent().setClassName(cordova.getActivity(), className);
 		cordova.startActivityForResult(this, intent, 1);
-		callbackContext.success("Hahaha!");
+		//callbackContext.success("Hahaha!");
 
 		return true;
 	}
@@ -36,6 +41,21 @@ public class Demo extends CordovaPlugin {
 		callbackContext.success();
 
 		return true;
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		switch (resultCode) {
+			case Activity.RESULT_OK:
+				Bundle bundle = intent.getExtras();
+				String result = bundle.getString("result");
+				m_callbackContext.success(result);
+				break;
+
+			default:
+				m_callbackContext.success();
+				break;
+		}
 	}
 
 }
--- src/android/DemoActivity.java
+++ src/android/DemoActivity.java
@@ -1,7 +1,17 @@
 package cordova.plugins.demo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class DemoActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = new Intent().putExtra("result", "Heiheihei!");
+		setResult(Activity.RESULT_OK, intent);
+		finish();
+	}
 }
