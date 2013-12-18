936ba937d0d8d17481f39fe1e432503c78f02c83
redv1981 <redv1981@gmail.com>
2013-12-18 09:52:32 +0800

    1

--- src/android/DemoActivity.java
+++ src/android/DemoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.widget.Button;
 
 import java.io.File;
 
@@ -13,10 +14,14 @@ public class DemoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		Button btn = new Button(this);
+		
+/*
 		File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
 		Intent intent = new Intent().putExtra("result", path.toString());
 		setResult(Activity.RESULT_OK, intent);
 		finish();
+*/
 	}
 
 }
